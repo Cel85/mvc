f472485ff41a2b17d6c81dcdf2d341e95e8f4405
MarkoRaseta <marko.raseta@moj-posao.net>
2019-02-21 10:42:38 +0100

    more setup, added classes

--- app/core/App.php
+++ app/core/App.php
@@ -1,7 +1,21 @@
 <?php
 
 class App {
+
+	protected $controller = 'home';
+	protected $method = 'index';
+	protected $params = [];
+
 	public function __construct(){
-		echo "radi";
+		echo "radi<br />";
+		echo "parsam url: <br />";
+		$this->parseUrl();
+
+	}
+
+	public function parseUrl(){
+		if(isset($_GET['url'])){
+			echo $_GET['url'];
+		}
 	}
 }
